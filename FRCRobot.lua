a67e5007af291db7eaffc0e4667e1b1f392c187e
Steve Tarr <stevet@RHUBARB>
2013-01-11 11:46:16 -0800

    decode Kinect headers in extension data

--- FRCRobot.lua
+++ FRCRobot.lua
@@ -8,7 +8,7 @@ do
     return x % (p + p) >= p       
   end
 
-  -- create DSEIO output protocol and its fields
+  -- DSEIO output protocol and its fields
   p_eioout = Proto ("eioout","Driver Station Enhanced I/O Output")
 
   local f_eioout_size       = ProtoField.uint16("eioout.size","size", base.DEC)
@@ -29,10 +29,30 @@ do
   local f_eioout_fixed      = ProtoField.uint8("eioout.fixed","fixed digital out", base.BIN)
   local f_eioout_flags      = ProtoField.uint8("eioout.flags","flags", base.HEX)
 
+  p_eioout.fields = {
+    f_eioout_size,
+    f_eioout_id,
+    f_eioout_dio,
+    f_eioout_oe,
+    f_eioout_pe,
+    f_eioout_pwm_cmp_1,
+    f_eioout_pwm_cmp_2,
+    f_eioout_pwm_cmp_3,
+    f_eioout_pwm_cmp_4,
+    f_eioout_pwm_per_1,
+    f_eioout_pwm_per_2,
+    f_eioout_dac_1,
+    f_eioout_dac_2,
+    f_eioout_leds,
+    f_eioout_enables,
+    f_eioout_fixed,
+    f_eioout_flags
+  }
+
   function p_eioout.dissector (buf, pkt, root)
     if buf:len() < 25 then return end
     pkt.cols.protocol = p_eioout.name
-    subtree = root:add(p_eioout, buf(0))
+    local subtree = root:add(p_eioout, buf(0))
 
     subtree:add(f_eioout_size, buf(0,1))
     subtree:add(f_eioout_id, buf(1,1))
@@ -50,12 +70,12 @@ do
     subtree:add(f_eioout_dac_2, buf(21,1))
     subtree:add(f_eioout_leds, buf(22,1))
     subtree:add(f_eioout_enables, buf(23,1))
-      -- enables should be broken into subfields
+    -- enables should be broken into subfields
     subtree:add(f_eioout_fixed, buf(24,1))
     subtree:add(f_eioout_flags, buf(25,1))
   end
 
-  -- create Robot-to-DS protocol and its fields
+  -- Robot-to-DS protocol and its fields
   p_r2d = Proto ("r2d","Robot To Driver Station Protocol")
 
   local f_r2d_control = ProtoField.uint8("r2d.command", "Control", base.HEX)
@@ -73,17 +93,30 @@ do
   local f_r2d_unknown_3 = ProtoField.bytes("r2d.unknown3", "Unknown 3")
   local f_r2d_crc     = ProtoField.uint32("r2d.crc", "CRC", base.HEX)
 
-  p_r2d.fields = {f_r2d_control, f_r2d_battery, f_r2d_output, f_r2d_unknown_1, f_r2d_team,
-  		  f_r2d_mac, f_r2d_version, f_r2d_unknown_2, f_r2d_packet, f_r2d_update,
-		  f_r2d_user, f_r2d_eio, f_r2d_unknown_3, f_r2d_crc}
+  p_r2d.fields = {
+    f_r2d_control,
+    f_r2d_battery,
+    f_r2d_output,
+    f_r2d_unknown_1,
+    f_r2d_team,
+    f_r2d_mac,
+    f_r2d_version,
+    f_r2d_unknown_2,
+    f_r2d_packet,
+    f_r2d_update,
+    f_r2d_user,
+    f_r2d_eio,
+    f_r2d_unknown_3,
+    f_r2d_crc
+  }
 
   function p_r2d.dissector (buf, pkt, root)
     if buf:len() < 1024 then return end
     pkt.cols.protocol = p_r2d.name
-    subtree = root:add(p_r2d, buf(0))
+    local subtree = root:add(p_r2d, buf(0))
     
-    val = buf(0,1):uint()
-    ctrl = subtree:add(f_r2d_control, buf(0,1))
+    local val = buf(0,1):uint()
+    local ctrl = subtree:add(f_r2d_control, buf(0,1))
     ctrl:add("Reset:         " .. (hasbit(val, bit(7)) and "Set" or "Unset"))
     ctrl:add("!EStop:        " .. (hasbit(val, bit(6)) and "Set" or "Unset"))
     ctrl:add("Enabled:       " .. (hasbit(val, bit(5)) and "Set" or "Unset"))
@@ -93,14 +126,14 @@ do
     ctrl:add("CRio Checksum: " .. (hasbit(val, bit(1)) and "Set" or "Unset"))
     ctrl:add("FPGA Checksum: " .. (hasbit(val, bit(0)) and "Set" or "Unset"))
     
-    batt_v = buf(1,1):uint()
-    batt_mv = buf(2,1):uint()
-    batt = subtree:add(f_r2d_battery, buf(1,2))
+    local batt_v = buf(1,1):uint()
+    local batt_mv = buf(2,1):uint()
+    local batt = subtree:add(f_r2d_battery, buf(1,2))
     batt:add("Volts: "..batt_v)
     batt:add("Milivolts: "..batt_mv)
     
     val = buf(3,1):uint()
-    out = subtree:add(f_r2d_output, buf(3, 1))
+    local out = subtree:add(f_r2d_output, buf(3, 1))
     out:add("Output 1: " .. (hasbit(val, bit(0)) and "Set" or "Unset"))
     out:add("Output 2: " .. (hasbit(val, bit(1)) and "Set" or "Unset"))
     out:add("Output 3: " .. (hasbit(val, bit(2)) and "Set" or "Unset"))
@@ -119,8 +152,8 @@ do
     subtree:add(f_r2d_update, buf(32,1))
     subtree:add(f_r2d_user, buf(33, 951))
 
-    eio = subtree:add(f_r2d_eio, buf(984,32))
-    eioout_size = buf(984,1):uint()
+    local eio = subtree:add(f_r2d_eio, buf(984,32))
+    local eioout_size = buf(984,1):uint()
     if (eioout_size > 0) then
 	p_eioout:call(buf(984,eioout_size+1), pkt, eio)
     end
@@ -129,7 +162,7 @@ do
     subtree:add(f_r2d_crc, buf(1020, 4))
   end
 
-  -- create DS-to-Robot protocol and its fields
+  -- DS-to-Robot protocol and its fields
   p_d2r = Proto ("d2r","Driver Station To Robot Protocol")
 
   local f_d2r_packet    = ProtoField.uint16("d2r.packet", "Packet Index", base.DEC)
@@ -150,29 +183,72 @@ do
   local f_d2r_analog2   = ProtoField.uint16("d2r.analog2", "Analog 2", base.DEC)
   local f_d2r_analog3   = ProtoField.uint16("d2r.analog3", "Analog 3", base.DEC)
   local f_d2r_analog4   = ProtoField.uint16("d2r.analog4", "Analog 4", base.DEC)
-  local f_d2r_crio_chk  = ProtoField.uint64("d2r.crio_chk", "cRio Checksum", base.HEX)
+  local f_d2r_crio_chk  = ProtoField.uint64("d2r.crio_chk", "cRIO Checksum", base.HEX)
   local f_d2r_fpga_chk1 = ProtoField.uint32("d2r.fpga_chk1", "FPGA Checksum 1", base.HEX)
   local f_d2r_fpga_chk2 = ProtoField.uint32("d2r.fpga_chk2", "FPGA Checksum 2", base.HEX)
   local f_d2r_fpga_chk3 = ProtoField.uint32("d2r.fpga_chk3", "FPGA Checksum 3", base.HEX)
   local f_d2r_fpga_chk4 = ProtoField.uint32("d2r.fpga_chk4", "FPGA Checksum 4", base.HEX)
   local f_d2r_version   = ProtoField.string("d2r.version", "Version")
-  local f_d2r_high      = ProtoField.bytes("d2r.high", "High Priority Data")
+  local f_d2r_ext       = ProtoField.bytes("d2r.ext", "Extension")
+  local f_d2r_ext_len	= ProtoField.uint8("d2r.ext.len", "Extension Length", base.DEC)
+  local d2r_ext_tags = {
+    [17] = "DS Enhanced I/O Input",
+    [18] = "DS Enhanced I/O Output",
+    [19] = "Kinect_Header",
+    [20] = "Kinect_Extra1",
+    [21] = "Kinect_Vertices1",
+    [22] = "Kinect_Extra2",
+    [23] = "Kinect_Vertices2",
+    [24] = "Kinect_Joystick",
+    [25] = "Kinect_Custom"
+  }
+  local f_d2r_ext_tag   = ProtoField.uint8("d2r.ext.tag", "Extension Tag", base.DEC, d2r_ext_tags)
+
+  local f_d2r_ext_val	= ProtoField.bytes("d2r.ext.val", "Extension Data")
   local f_d2r_crc       = ProtoField.uint32("d2r.crc", "CRC", base.HEX)
 
-  p_d2r.fields = {f_d2r_packet, f_d2r_control, f_d2r_input, f_d2r_team, f_d2r_alliance, f_d2r_position,
-		  f_d2r_joystick1, f_d2r_js_btn1, f_d2r_joystick2, f_d2r_js_btn2, f_d2r_joystick3, f_d2r_js_btn3,
-		  f_d2r_joystick4, f_d2r_js_btn4, f_d2r_analog1, f_d2r_analog2, f_d2r_analog3, f_d2r_analog4,
-		  f_d2r_crio_check, f_d2r_fpga_chk1, f_d2r_fpga_chk2, f_d2r_fpga_chk3, f_d2r_fpga_chk4,
-		  f_d2r_version, f_d2r_high, f_d2r_crc}
+  p_d2r.fields = {
+    f_d2r_packet,
+    f_d2r_control,
+    f_d2r_input,
+    f_d2r_team,
+    f_d2r_alliance,
+    f_d2r_position,
+    f_d2r_joystick1,
+    f_d2r_js_btn1,
+    f_d2r_joystick2,
+    f_d2r_js_btn2,
+    f_d2r_joystick3,
+    f_d2r_js_btn3,
+    f_d2r_joystick4,
+    f_d2r_js_btn4,
+    f_d2r_analog1,
+    f_d2r_analog2,
+    f_d2r_analog3,
+    f_d2r_analog4,
+    f_d2r_crio_check,
+    f_d2r_fpga_chk1,
+    f_d2r_fpga_chk2,
+    f_d2r_fpga_chk3,
+    f_d2r_fpga_chk4,
+    f_d2r_version,
+    f_d2r_ext,
+    f_d2r_ext_len,
+    f_d2r_ext_tag,
+    f_d2r_ext_val,
+    f_d2r_crc
+  }
 
   function p_d2r.dissector (buf, pkt, root)
     if buf:len() < 1024 then return end
     pkt.cols.protocol = p_d2r.name
-    subtree = root:add(p_d2r, buf(0))
+
+    local subtree = root:add(p_d2r, buf(0))
+
     subtree:add(f_d2r_packet, buf(0,2))
     
-    val = buf(2,1):uint()
-    ctrl = subtree:add(f_d2r_control, buf(2,1))
+    local val = buf(2,1):uint()
+    local ctrl = subtree:add(f_d2r_control, buf(2,1))
     ctrl:add("Reset:         " .. (hasbit(val, bit(7)) and "Set" or "Unset"))
     ctrl:add("!EStop:        " .. (hasbit(val, bit(6)) and "Set" or "Unset"))
     ctrl:add("Enabled:       " .. (hasbit(val, bit(5)) and "Set" or "Unset"))
@@ -183,7 +259,7 @@ do
     ctrl:add("FPGA Checksum: " .. (hasbit(val, bit(0)) and "Set" or "Unset"))
     
     val = buf(3,1):uint()
-    out = subtree:add(f_d2r_input, buf(3, 1))
+    local out = subtree:add(f_d2r_input, buf(3, 1))
     out:add("Input 1: " .. (hasbit(val, bit(0)) and "Set" or "Unset"))
     out:add("Input 2: " .. (hasbit(val, bit(1)) and "Set" or "Unset"))
     out:add("Input 3: " .. (hasbit(val, bit(2)) and "Set" or "Unset"))
@@ -196,8 +272,10 @@ do
     subtree:add(f_d2r_team, buf(4, 2))
     subtree:add(f_d2r_alliance, buf(6,1))
     subtree:add(f_d2r_position, buf(7,1))
+
+    local driver = subtree:add("driver controls")
     
-    js1 = subtree:add(f_d2r_joystick1, buf(8, 6))
+    local js1 = driver:add(f_d2r_joystick1, buf(8, 6))
     js1:add(f_d2r_js_btn1, buf(14, 2))
     js1:add("Axis 1: " .. buf(8,1))
     js1:add("Axis 2: " .. buf(9,1))
@@ -206,7 +284,7 @@ do
     js1:add("Axis 5: " .. buf(12,1))
     js1:add("Axis 6: " .. buf(13,1))
     
-    js2 = subtree:add(f_d2r_joystick2, buf(16, 6))
+    local js2 = driver:add(f_d2r_joystick2, buf(16, 6))
     js2:add(f_d2r_js_btn2, buf(22, 2))
     js2:add("Axis 1: " .. buf(16,1))
     js2:add("Axis 2: " .. buf(17,1))
@@ -215,7 +293,7 @@ do
     js2:add("Axis 5: " .. buf(20,1))
     js2:add("Axis 6: " .. buf(21,1))
     
-    js3 = subtree:add(f_d2r_joystick3, buf(24, 6))
+    local js3 = driver:add(f_d2r_joystick3, buf(24, 6))
     js3:add(f_d2r_js_btn3, buf(30, 2))
     js3:add("Axis 1: " .. buf(24,1))
     js3:add("Axis 2: " .. buf(25,1))
@@ -224,7 +302,7 @@ do
     js3:add("Axis 5: " .. buf(28,1))
     js3:add("Axis 6: " .. buf(29,1))
     
-    js4 = subtree:add(f_d2r_joystick4, buf(32, 6))
+    local js4 = driver:add(f_d2r_joystick4, buf(32, 6))
     js4:add(f_d2r_js_btn4, buf(38, 2))
     js4:add("Axis 1: " .. buf(32,1))
     js4:add("Axis 2: " .. buf(33,1))
@@ -233,47 +311,51 @@ do
     js4:add("Axis 5: " .. buf(36,1))
     js4:add("Axis 6: " .. buf(37,1))
     
-    subtree:add(f_d2r_analog1, buf(40, 2))
-    subtree:add(f_d2r_analog2, buf(42, 2))
-    subtree:add(f_d2r_analog3, buf(44, 2))
-    subtree:add(f_d2r_analog4, buf(46, 2))
+    driver:add(f_d2r_analog1, buf(40, 2))
+    driver:add(f_d2r_analog2, buf(42, 2))
+    driver:add(f_d2r_analog3, buf(44, 2))
+    driver:add(f_d2r_analog4, buf(46, 2))
     
-    subtree:add(f_d2r_crio_chk, buf(48, 8))
-    subtree:add(f_d2r_fpga_chk1, buf(56, 4))
-    subtree:add(f_d2r_fpga_chk2, buf(60, 4))
-    subtree:add(f_d2r_fpga_chk3, buf(64, 4))
-    subtree:add(f_d2r_fpga_chk4, buf(68, 4))
+    local checksums = subtree:add("checksums")
+
+    checksums:add(f_d2r_crio_chk, buf(48, 8))
+
+    checksums:add(f_d2r_fpga_chk1, buf(56, 4))
+    checksums:add(f_d2r_fpga_chk2, buf(60, 4))
+    checksums:add(f_d2r_fpga_chk3, buf(64, 4))
+    checksums:add(f_d2r_fpga_chk4, buf(68, 4))
     
-    subtree:add(f_d2r_version, buf(72, 8))
+    checksums:add(f_d2r_version, buf(72, 8))
     
-    subtree:add(f_d2r_high, buf(80, 940))
+    local ext = subtree:add(f_d2r_ext, buf(80))
+    local ext_off = 80
+    local ext_len = 0
+    local ext_tag = 0
+    local ext_val
+    local ext_tlv
+    while ext_off < 1020 do
+	ext_len = buf(ext_off, 1):uint()
+	if ext_len == 0 then break end
+	ext_tag = buf(ext_off+1, 1):uint()
+	if ext_tag == 0 then break end
+	ext_val = buf(ext_off+2, ext_len-1)
+	ext_tlv = ext:add("len " .. ext_len .. " tag " .. ext_tag .. " val " .. ext_val, buf(ext_off,ext_len))
+	ext_tlv:add(f_d2r_ext_len, buf(ext_off,1))
+	ext_tlv:add(f_d2r_ext_tag, buf(ext_off+1,1))
+	if ext_len > 1 then
+	    ext_tlv:add(f_d2r_ext_val, buf(ext_off+2,ext_len-1))
+	end
+	ext_off = ext_off + 1 + ext_len
+    end
     subtree:add(f_d2r_crc, buf(1020, 4))
   end
 
-  -- define wrapper to call the correct dissector
-  p_frc = Proto ("FRC","FRC Robot Control Protocol")
+  -- install dissectors
   local FRC_ROBOT_PORT = 1110
-  local f_udp_srcport = Field.new("udp.srcport")
-  local f_udp_dstport = Field.new("udp.dstport")
-  local original_dissector
-
-  function p_frc.dissector (buf, pkt, root)
-
-    pcall(
-	function()
-	    original_dissector:call(buf, pkt, root)
-	end
-    )
-
-    if (f_udp_srcport().value == FRC_ROBOT_PORT) then
-	p_r2d.dissector:call(buf, pkt, root)
-    else if (f_udp_dstport().value == FRC_ROBOT_PORT) then
-	p_d2r.dissector:call(buf, pkt, root)
-    end end
-  end
+  local FRC_DS_PORT = 1150
 
   local udp_dissector_table = DissectorTable.get("udp.port")
-  original_dissector = udp_dissector_table:get_dissector(FRC_ROBOT_PORT)
-  udp_dissector_table:add(FRC_ROBOT_PORT,p_frc)
+  udp_dissector_table:add(FRC_ROBOT_PORT,p_d2r)
+  udp_dissector_table:add(FRC_DS_PORT,p_r2d)
 end
 
